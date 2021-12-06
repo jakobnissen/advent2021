76655e9b91cabb77c70714dfda33c80cd815d6ab
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-12-06 12:56:56 +0100

    Optimize day06

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,17 +1,27 @@
 pub fn solve(s: &str) -> (usize, usize) {
-    let mut fishes: [usize; 9] = [0; 9];
-    s.trim()
-        .split(',')
-        .for_each(|s| fishes[s.parse::<usize>().unwrap()] += 1);
-    let mut part1 = 0;
-    for _day in 0..256 {
-        fishes[7] += fishes[0];
-        fishes.rotate_left(1);
-        if _day == 79 {
-            part1 = fishes.iter().sum();
+    let lut: [(usize, usize); 7] = [
+        (1421, 6703087164),
+        (1401, 6206821033),
+        (1191, 5617089148),
+        (1154, 5217223242),
+        (1034, 4726100874),
+        ( 950, 4368232009),
+        ( 905, 3989468462)
+    ];
+    s.trim().as_bytes().iter().enumerate().fold((0, 0), |(p1, p2), (i, &b)| {
+        if i % 2 == 1 {
+            if b != b',' {
+                panic!()
+            }
+            return (p1, p2)
         }
-    }
-    (part1, fishes.iter().sum())
+        let n = b - b'0';
+        if !(0x01..0x08).contains(&n) {
+            panic!()
+        }
+        let (a1, a2) = lut[n as usize];
+        (p1 + a1, p2 + a2)
+    })
 }
 
 #[cfg(test)]
